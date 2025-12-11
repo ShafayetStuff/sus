f987b607f650883784e59ffa036732e1ad95caeb
ShafayetStuff <mdshafayatislamifaz13@gmail.com>
2025-12-11 14:42:30 +0600

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -24,18 +24,24 @@ const validName = [
     'hasin',
     'rashed',
 ]
-function checkinput(){
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkinput(){
     const value = document.getElementById("inp").value.toLowerCase();
+    
     if(validName.includes(value)){
-        window.alert("After analysing the name in our database " + value + " is Gay")
+        window.alert("After analysing the name in our database " + value + " is Gay");
     }
     else if(value === ""){
-        window.alert("Enter some shi")
+        window.alert("Enter some shi");
     }
-       else if(value.includes(" ")){
-        window.alert("Enter some shi")
+    else if(value.includes(" ")){
+        window.alert("Dont use spaces and full name, just use first name or the name only people use to call you");
     }
     else{
-        window.alert("After analysing the name in our database " +value + " is straighter than the pole his/her's gay friend's mom dances on")
+        window.alert("After analysing the name in our database " + value + " is straighter than the pole his/her's gay friend's mom dances on");
     }
 }
